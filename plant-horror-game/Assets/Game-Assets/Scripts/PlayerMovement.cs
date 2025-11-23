33827b92f8e1ff9153fc7d8739d2194ce25a6072
gtamb <gillian.tamblyn@icloud.com>
2025-11-22 22:19:14 -0800

    Fixed movement and rotation

--- plant-horror-game/Assets/Game-Assets/Scripts/PlayerMovement.cs
+++ plant-horror-game/Assets/Game-Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,13 @@
-
-
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController controller;
-    public float playerSpeed = 5.0f;
-    public float jumpHeight = 1.5f;
+    public CharacterController controller;
+    public float playerSpeed = 2.0f;
+    public float rotationSpeed = 180f;
+    public float jumpHeight = 1f;
     public float gravityValue = -9.81f;
+    private Vector3 rotation;
 
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -24,16 +24,23 @@ public class PlayerMovement : MonoBehaviour
         {
             playerVelocity.y = 0f; // Reset vertical velocity if grounded and moving downwards
         }
+        
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+
+        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        move = this.transform.TransformDirection(move);
+        controller.Move(move * playerSpeed);
+        this.transform.Rotate(this.rotation);
 
         // Get Input for movement
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * playerSpeed * Time.deltaTime);
+        // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // controller.Move(move * playerSpeed * Time.deltaTime);
 
         // Apply rotation based on movement direction
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        // if (move != Vector3.zero)
+        // {
+        //     gameObject.transform.forward = move;
+        // }
 
         // Handle Jumping
         if (Input.GetButtonDown("Jump") && groundedPlayer)
@@ -44,5 +51,7 @@ public class PlayerMovement : MonoBehaviour
         // Apply Gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+   
     }
+
 }
