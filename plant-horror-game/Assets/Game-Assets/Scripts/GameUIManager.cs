6b886319d9f9c23aee45e3bf8dfc0e4466b83a3a
gtamb <gillian.tamblyn@icloud.com>
2025-11-23 21:11:38 -0800

    Added Health UI
    Added health UI to player, updates with damage. Need to adjust health damage settings with game.

---
+++ plant-horror-game/Assets/Game-Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameUIManager : MonoBehaviour
+{
+    public PlayerHealth ph;
+    public UIDocument uiDoc;
+    private Label healthLabel;
+    private VisualElement healthBarMask;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        healthLabel = uiDoc.rootVisualElement.Q<Label>("HealthLabel");
+        healthBarMask = uiDoc.rootVisualElement.Q<VisualElement>("HealthBarFill");
+        InvokeRepeating("healthChange", 0f, 0.5f); // Update health display every 0.5 seconds
+    }
+
+    // Update is called once per frame
+    void healthChange()
+    {
+        float healthRatio = (float)ph.curHealth / ph.maxHealth;
+        float healthPercent = Mathf.Lerp(0, 100, healthRatio);
+        healthBarMask.style.width = Length.Percent(healthPercent);
+
+        healthLabel.text = $"{ph.curHealth}/{ph.maxHealth}";
+
+    }
+}
